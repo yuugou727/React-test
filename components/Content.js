fffe869ec894fcb4d029a11e07f28cab385acaea
Ronnie <ronniechang@hyxen.com>
2016-11-25 15:44:31 +0800

    Layout

--- components/Content.js
+++ components/Content.js
@@ -6,11 +6,11 @@ export default class Content extends React.Component {
     render() {
         return (
             <div className="content container">
-			<div className="row">
-				<h3>Content</h3>
-				<Card />
-				<Card />
-			</div>
+            <h3>Content</h3>
+				<div className="row">	
+					<Card />
+					<Card />
+				</div>
 			</div>
         );
     }
